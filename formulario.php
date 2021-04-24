f9e1ac08d8adf3a21b0eebf9120e5a312bc7109d
MrDfg27 <dfgtoledo27@outlook.es>
2021-04-23 22:04:01 -0500

    8

--- formulario.php
+++ formulario.php
@@ -9,6 +9,11 @@ $bono = $_POST['bono'];
 $ingreso = $_POST['ingreso'];
 
 
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
 $message = "Este mensaje fue enviado por: " . $name . " \r\n";
 $message .= "Su correo electronico es: " . $mail . " \r\n";
 $message .= "Teléfono de contacto es: " . $phone . " \r\n";
@@ -16,12 +21,11 @@ $message .= "Lugar de residencia: " . $vive . " \r\n";
 $message .= "Cuenta con lote propio: " . $lote . " \r\n";
 $message .= "Su tipo de bono es: " . $bono . " \r\n";
 $message .= "Su rango de ingreso familiar es: " . $ingreso . " \r\n";
-$message .= "Enviado el: " . date('d/m/Y', time());
 
 $para = 'dfgtoledo27@outlook.es';
 $asunto = 'Prueba pagina';
 
-mail($para, $asunto, $message);
+mail($para, $asunto, $message, $header);
 
 
 ?>
